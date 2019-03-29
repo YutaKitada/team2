0ee0b93845f99ff0d15742771f12325fd74f02f7
syake <->
2019-03-29 14:32:14 +0900

    SelectButtonのプレハブの追加,ローディングの画面の追加

---
+++ team2Game/Assets/Script/Scene/SelectButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class SelectButton : MonoBehaviour
+{
+    [SerializeField]
+    private Button[] buttons;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttons[0].Select();
+    }
+    
+}
