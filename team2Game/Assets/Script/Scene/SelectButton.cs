0cbfb35b876cd480dbbab5cd10943a661fb94c4a
syake <->
2019-04-19 15:47:28 +0900

    Pause画面の追加、ステージボタンのプレハブ追加

--- team2Game/Assets/Script/Scene/SelectButton.cs
+++ team2Game/Assets/Script/Scene/SelectButton.cs
@@ -10,11 +10,26 @@ public class SelectButton : MonoBehaviour
     [SerializeField]
     private Button[] buttons;
 
+    public static int currentButton = -1;    //最後に押されたボタン
 
     // Start is called before the first frame update
     void Start()
     {
-        buttons[0].Select();
+        //ゲーム開始時は0番目のボタンが初期選択状態
+        //currentButtonの値で変化
+        if (currentButton == -1)
+        {
+            buttons[0].Select();
+        }
+        else
+        {
+            buttons[currentButton].Select();
+        }
+    }
+    //インスペクターで変更可(ボタンの配列順で番号を指定)
+    public void OnClick(int num)
+    {
+        currentButton = num;
     }
     
 }
