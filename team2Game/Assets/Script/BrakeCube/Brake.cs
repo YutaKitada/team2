e4c53be69d843cfbe0085654c5ed82512f0c0147
丹下輝巳 <->
2019-05-10 14:46:25 +0900

    自分が作ったギミックをプレハブ化

---
+++ team2Game/Assets/Script/BrakeCube/Brake.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brake : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)//あたったとき
+    {
+        if (collision.transform.tag == "Star")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ team2Game/Assets/Script/FallCube/FallCube.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallCube : MonoBehaviour
+{
+    public float FallTime;//落ちるまでの時間
+    private bool FallFlag;//フラグ
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FallTime = 0;
+        FallFlag = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (FallFlag)
+        {
+            FallTime += Time.deltaTime;
+        }
+
+        if (FallTime > 3)//Playerが触れて３びょうが経過したら落ちる
+        {
+            transform.position += new Vector3(0, -9.8f, 0) * Time.deltaTime;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)//あたったとき
+    {
+        if (collision.transform.tag == "Player")
+        {
+            FallFlag = true;
+        }
+    }
+}
