230c29411d1445e638cc1bf7aaac944c4d0c0083
ヘルシー北田 <kitada.k017a1491@gmail.com>
2019-05-21 10:45:37 +0900

    シェーダーの改良

---
+++ team2Game/Assets/Script/Player/KakushiCommand.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class KakushiCommand : MonoBehaviour
+{
+    int cmdSeq = 0;
+    int[] keyCodes;
+    int[] konamiCommand = new[] {
+        (int)KeyCode.UpArrow,
+        (int)KeyCode.UpArrow,
+        (int)KeyCode.DownArrow,
+        (int)KeyCode.DownArrow,
+        (int)KeyCode.LeftArrow,
+        (int)KeyCode.RightArrow,
+        (int)KeyCode.LeftArrow,
+        (int)KeyCode.RightArrow,
+        (int)KeyCode.B,
+        (int)KeyCode.A
+    };
+    int kcnt = 0;
+
+    [SerializeField]
+    private GameObject spaceCancer;
+
+    private void Start()
+    {
+        keyCodes = (int[])Enum.GetValues(typeof(KeyCode));
+    }
+
+    void Update()
+    {
+        var len = keyCodes.Length;
+        for (var i = 0; i < len; i++)
+        {
+            if (Input.GetKeyUp((KeyCode)keyCodes[i]))
+            {
+                if (konamiCommand[cmdSeq] == keyCodes[i])
+                {
+                    cmdSeq++;
+                    if (cmdSeq == konamiCommand.Length)
+                    {
+                        kcnt++;
+                        Instantiate(spaceCancer, GameManager.player.transform.position + new Vector3(0, 15), Quaternion.identity);
+                        cmdSeq = 0;
+                    }
+                }
+                else
+                {
+                    cmdSeq = 0;
+                }
+            }
+        }
+    }
+}
