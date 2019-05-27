6b9d7e521f03ab092a9d68689ffa91b672f68590
誠司 <ms01ps02@gmail.com>
2019-05-27 11:21:06 +0900

    タウラスの死亡時のアニメーションの不具合の修正 カメラの振動を追加

---
+++ team2Game/Assets/Script/Enemy/ShakeCamera.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShakeCamera : MonoBehaviour
+{
+    [SerializeField, Header("振動時間")]
+    float shakeTime = 0.5f;
+
+    float lifeTime;
+    Vector3 savePposition;
+    float lowRangeX;
+    float maxRangeX;
+    float lowRangeY;
+    float maxRangeY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(shakeTime <= 0)
+        {
+            shakeTime = 0.7f;
+        }
+        lifeTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lifeTime < 0)
+        {
+            transform.position = savePposition;
+            lifeTime = 0;
+        }
+
+        if (lifeTime > 0)
+        {
+            lifeTime -= Time.deltaTime;
+            float x_value = Random.Range(lowRangeX, maxRangeX);
+            float y_value = Random.Range(lowRangeY, maxRangeY);
+            transform.position = new Vector3(x_value, y_value, transform.position.z);
+        }
+    }
+
+    public void Shake()
+    {
+        savePposition = transform.position;
+        lowRangeY = savePposition.y - 1f;
+        maxRangeY = savePposition.y + 1f;
+        lowRangeX = savePposition.x - 1f;
+        maxRangeX = savePposition.x + 1f;
+        lifeTime = shakeTime;
+    }
+}
